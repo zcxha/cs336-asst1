f7b7847446943e0a1a661119e44ed9f68a8b1d8d
zcxcut <a1584537295@outlook.com>
2026-07-31 14:54:42 +0800

    post-norm ablation

--- cs336_basics/prenorm_transformer_block.py
+++ cs336_basics/prenorm_transformer_block.py
@@ -203,11 +203,10 @@ class TransformerBlock(torch.nn.Module):
         batch_size = x.shape[0]
         token_positions = torch.arange(0, seq_len, device=x.device).unsqueeze(0).unsqueeze(0).expand([batch_size, 1, seq_len])
         
-        attention_with_rope = self.attn(self.ln1(x), token_positions) + x
+        z = self.ln1(self.attn(x, token_positions) + x)
+        y = self.ln2(self.ffn(z) + z)
 
-        ffn_result = self.ffn(self.ln2(attention_with_rope)) + attention_with_rope
-
-        return ffn_result
+        return y
 
 class TransformerLM(torch.nn.Module):
     r"""
